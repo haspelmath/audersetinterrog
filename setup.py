0b22e1ddab096f68706e52bfe95a2d8d154f6290
chrzyki <christoph@functor-argument.net>
2020-02-20 16:44:41 +0100

    Add skeleton, add raw data

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_audersetinterrog',
+    py_modules=['cldfbench_audersetinterrog'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'audersetinterrog=cldfbench_audersetinterrog:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
