72bdbd2e01a4117b5e6ca5f6ad774033b17b1256
Johannes Englisch <johannes_englisch@eva.mpg.de>
2023-01-09 12:15:37 +0100

    Reshape data into crossgram-style dataset.
     * consider each relative marker a Construction
     * turn parameters into Construction Parameters
     * move forms out of the example table
     * make a summary feature for rel. markers for each language

--- cldfbench_audersetinterrog.py
+++ cldfbench_audersetinterrog.py
@@ -1,5 +1,4 @@
 import collections
-import operator
 import pathlib
 
 from cldfbench import CLDFSpec
@@ -15,12 +14,91 @@ class Dataset(BaseDataset):
         return CLDFSpec(dir=self.cldf_dir, module="StructureDataset")
 
     def cmd_makecldf(self, args):
-        parameters = self.etc_dir.read_csv("parameters.csv", dicts=True)
-        codes = self.etc_dir.read_csv("codes.csv", dicts=True)
+
+        # read raw data
+
+        parameters = list(self.etc_dir.read_csv("parameters.csv", dicts=True))
+        codes_by_id = collections.OrderedDict(
+            (row['ID'], row)
+            for row in self.etc_dir.read_csv('codes.csv', dicts=True))
+
+        raw_data = list(self.raw_dir.read_csv(
+            "InterrogativeRelativeIE_Appendix1.csv", dicts=True))
+
+        # knead into a cldfifiable shape
+
+        languages_by_id = collections.OrderedDict()
+        for row in raw_data:
+            if row['Glottocode'] not in languages_by_id:
+                languages_by_id[row['Glottocode']] = {
+                    'ID': row['Glottocode'],
+                    'Glottocode': row['Glottocode'],
+                    'Name': row['Language'],
+                    'Family': row['Branch'],
+                    'Subbranch': row['Subbranch'],
+                    'Subsubbranch': row['Subsubbranch'],
+                    'EarlyTimeBP': row['EarlyTimeBP'],
+                    'LateTimeBP': row['LateTimeBP'],
+                    'AvTimeBP': row['AvTimeBP'],
+                    'Latitude': row['Latitude'],
+                    'Longitude': row['Longitude'],
+                }
+
+        constructions = [
+            {
+                'ID': row['ID'],
+                'Name': '{} relative pronoun {}'.format(
+                    languages_by_id[row['Glottocode']]['Name'],
+                    row['RMform']),
+                'Language_ID': row['Glottocode'],
+            }
+            for row in raw_data]
+
+        cvalues = []
+        for row in raw_data:
+            for parameter in parameters:
+                if parameter['ID'] == 'rmform':
+                    code_id = None
+                    comment = row['Notes']
+                else:
+                    code_id = '{}-{}'.format(
+                        parameter['ID'], slug(row[parameter['Name']]))
+                    comment = None
+                cvalue = {
+                    'ID': '{}-{}'.format(row['ID'], parameter['ID']),
+                    'Construction_ID': row['ID'],
+                    'Parameter_ID': parameter['ID'],
+                    'Value': row[parameter['Name']],
+                    'Code_ID': code_id,
+                    'Comment': comment,
+                }
+                cvalues.append(cvalue)
+
+        parameters.append({
+            'ID': 'rmforms',
+            'Name': 'RMForms',
+            'Description': 'collection of relative markers used in a language',
+        })
+        forms_by_language = collections.OrderedDict()
+        for row in raw_data:
+            glottocode = row['Glottocode']
+            if glottocode not in forms_by_language:
+                forms_by_language[glottocode] = []
+            forms_by_language[glottocode].append(row['RMform'])
+        values = [
+            {
+                'ID': 'rmforms-{}'.format(lang_id),
+                'Language_ID': lang_id,
+                'Parameter_ID': 'rmforms',
+                'Value': ' / '.join(forms),
+            }
+            for lang_id, forms in forms_by_language.items()]
+
+        # cldf output
 
         args.writer.cldf.add_component(
             "LanguageTable",
-            "Branch",
+            "Family",
             "Subbranch",
             "Subsubbranch",
             "EarlyTimeBP",
@@ -29,66 +107,28 @@ class Dataset(BaseDataset):
         )
         args.writer.cldf.add_component("ParameterTable")
         args.writer.cldf.add_component("CodeTable")
-        args.writer.cldf.add_component("ExampleTable")
+        args.writer.cldf.add_table(
+            'constructions.csv',
+            'http://cldf.clld.org/v1.0/terms.rdf#id',
+            'http://cldf.clld.org/v1.0/terms.rdf#languageReference',
+            'http://cldf.clld.org/v1.0/terms.rdf#name',
+            'http://cldf.clld.org/v1.0/terms.rdf#description')
+        args.writer.cldf.add_table(
+            'cvalues.csv',
+            'http://cldf.clld.org/v1.0/terms.rdf#id',
+            'Construction_ID',
+            'http://cldf.clld.org/v1.0/terms.rdf#parameterReference',
+            'http://cldf.clld.org/v1.0/terms.rdf#codeReference',
+            'http://cldf.clld.org/v1.0/terms.rdf#value',
+            'http://cldf.clld.org/v1.0/terms.rdf#comment')
 
-        lines_by_id = collections.OrderedDict(
-            [
-                (line["ID"], line)
-                for line in self.raw_dir.read_csv(
-                    "InterrogativeRelativeIE_Appendix1.csv", dicts=True
-                )
-            ]
-        )
+        args.writer.cldf.add_foreign_key(
+            'cvalues.csv', 'Construction_ID',
+            'constructions.csv', 'ID')
 
-        for lang in lines_by_id.values():
-            if not lang["Glottocode"] in map(
-                operator.itemgetter("ID"), args.writer.objects["LanguageTable"]
-            ):
-                args.writer.objects["LanguageTable"].append(
-                    dict(
-                        ID=lang["Glottocode"],
-                        Glottocode=lang["Glottocode"],
-                        Name=lang["Language"],
-                        Branch=lang["Branch"],
-                        Subbranch=lang["Subbranch"],
-                        Subsubbranch=lang["Subsubbranch"],
-                        EarlyTimeBP=lang["EarlyTimeBP"],
-                        LateTimeBP=lang["LateTimeBP"],
-                        AvTimeBP=lang["AvTimeBP"],
-                        Latitude=lang["Latitude"],
-                        Longitude=lang["Longitude"],
-                    )
-                )
-
-        for parameter in parameters:
-            args.writer.objects["ParameterTable"].append(
-                dict(
-                    ID=parameter["ID"], Name=parameter["Name"], Description=parameter["Description"]
-                )
-            )
-
-        for code in codes:
-            args.writer.objects["CodeTable"].append(
-                dict(ID=code["ID"], Parameter_ID=code["Parameter_ID"], Name=code["Name"])
-            )
-
-        for example in lines_by_id.values():
-            args.writer.objects["ExampleTable"].append(
-                dict(
-                    ID=example["ID"],
-                    Language_ID=example["Glottocode"],
-                    Primary_Text=example["RMform"],
-                )
-            )
-
-        for value in lines_by_id.values():
-            for parameter in parameters:
-                args.writer.objects["ValueTable"].append(
-                    dict(
-                        ID="{0}-{1}-{2}".format(value["Glottocode"], parameter["ID"], value["ID"]),
-                        Language_ID=value["Glottocode"],
-                        Parameter_ID=parameter["ID"],
-                        Value=value[parameter["Name"]],
-                        Code_ID="{0}-{1}".format(parameter["ID"], slug(value[parameter["Name"]])),
-                    )
-                )
+        args.writer.objects['LanguageTable'] = languages_by_id.values()
+        args.writer.objects['constructions.csv'] = constructions
+        args.writer.objects['ParameterTable'] = parameters
+        args.writer.objects['CodeTable'] = codes_by_id.values()
+        args.writer.objects['cvalues.csv'] = cvalues
+        args.writer.objects['values.csv'] = values
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 def test_extent(cldf_dataset):
     assert len(list(cldf_dataset["LanguageTable"])) == 99
-    assert len(list(cldf_dataset["ParameterTable"])) == 7
+    assert len(list(cldf_dataset["ParameterTable"])) == 9
     assert len(list(cldf_dataset["CodeTable"])) == 43
-    assert len(list(cldf_dataset["ExampleTable"])) == 150
-    assert len(list(cldf_dataset["ValueTable"])) == 1050
+    assert len(list(cldf_dataset["ValueTable"])) == 99
+    assert len(list(cldf_dataset["cvalues.csv"])) == 1200
